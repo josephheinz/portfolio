9c8605770cfc8029882fcc9977d3bc01bed9209b
Joseph Heinz <josephheinz28@gmail.com>
2025-05-22 10:44:37 -0400

    started on projects section

---
+++ src/lib/components/ProjectCard.svelte
@@ -0,0 +1,24 @@
+<script>
+    const { technologies } = $props();
+</script>
+
+<div
+	class="shadow-custom m-8 flex flex-col items-center justify-stretch gap-8 rounded-lg border-4 border-zinc-950 bg-amber-200 p-4"
+>
+	<div
+		class="shadow-custom aspect-video max-w-full overflow-hidden rounded-lg border-4 border-zinc-950"
+	>
+		<slot name="thumbnail" />
+	</div>
+	<h1 class="text-h4 font-bold">
+		<slot name="title" />
+	</h1>
+    <span class="text-para max-w-[350px] text-center">
+        <slot name="description" />
+    </span>
+    <div class="w-full flex items-center justify-center gap-1">
+        {#each technologies as tech}
+            <i class={`devicon-${tech}-plain colored`}></i>
+        {/each}
+    </div>  
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 	import VerticalDivider from '$lib/components/VerticalDivider.svelte';
 	import Button from '$lib/components/Button.svelte';
 	import SquiggleSpacer from '$lib/components/SquiggleSpacer.svelte';
+	import ProjectCard from '$lib/components/ProjectCard.svelte';
 </script>
 
 <Navbar>
@@ -36,3 +37,18 @@
 	<h1 class="text-h1 font-black">About Me</h1>
 </div>
 <SquiggleSpacer />
+<div class="flex h-min flex-col items-start justify-start px-40 py-16">
+	<h1 class="text-h1 font-black">Projects</h1>
+	<div class="grid w-full grid-cols-3 gap-12 p-6">
+		<!-- svelte-ignore a11y_missing_attribute -->
+		<ProjectCard technologies={['html', 'css', 'js']}
+			><img slot="thumbnail" src="headshot.png" />
+			<h1 slot="title">Project 1</h1>
+			<p slot="description">
+				A livestream site I created that uses my own RTMP server so I can stream directly to my own
+				website instead of running my livestream through an existing service such as Twitch or
+				YouTube.
+			</p>
+		</ProjectCard>
+	</div>
+</div>
