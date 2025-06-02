a192fa60ed91a0ea4fcddc8a534ca40ff1042653
Joseph Heinz <josephheinz28@gmail.com>
2025-06-02 10:42:47 -0400

    added gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,21 @@
-import { resolve } from 'path';
-const assets = resolve('src/app');
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 // Rest of your svelte.config.js code here
 import adapter from '@sveltejs/adapter-static';
-import { strict } from 'assert';
+const dev = process.env.NODE_ENV === 'development';
 
-const config = {
+
+export default {
     kit: {
         adapter: adapter({
-            strict: false,
             pages: 'build',
             assets: 'build',
-            fallback: undefined
+            fallback: 'index.html', // allow dynamic routes to load
+            strict: false            // suppress warnings about dynamic routes
         }),
-
+        paths: {
+            base: '/portfolio' // required for GH Pages project site
+        },
+        prerender: {
+            entries: [] // don't try to prerender routes
+        }
     }
 };
-
-export default config;
