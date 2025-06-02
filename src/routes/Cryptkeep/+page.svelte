99d28ae702ff68e4958a97387f72cd9b84fdee4c
Joseph Heinz <josephheinz28@gmail.com>
2025-06-02 10:22:46 -0400

    finished and started build settings

---
+++ src/routes/Cryptkeep/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { onMount } from 'svelte';
+    onMount(() => {
+        window.location.href = '/Cryptkeep/Final Project Godot.html';
+    })
+</script>
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,24 @@
-import adapter from '@sveltejs/adapter-static';
+import { resolve } from 'path';
+const assets = resolve('src/app');
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const config = { kit: { adapter: adapter() } };
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Rest of your svelte.config.js code here
+import adapter from '@sveltejs/adapter-static';
+import { strict } from 'assert';
+
+const config = {
+    kit: {
+        adapter: adapter({
+            strict: false,
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined
+        }),
+
+    }
+};
 
 export default config;
